59d155441a538b38fca0f99344b696374e06cdfa
Shna-hsn <58238805+Shna-hsn@users.noreply.github.com>
2020-12-22 14:12:08 +0800

    Delete connect.py

--- simple/connect.py
+++
@@ -1,8 +0,0 @@
-import cx_Oracle as cx
-
-connection = cx.connect('SFCFA139', 'SFCFA139', '10.41.129.33:1521/PLANT8')
-sql = 'SELECT * FROM TEMP_NANDER'
-cursor = connection.cursor()
-cursor.execute(sql)
-data = cursor.fetchall()
-print(data[0][0])
